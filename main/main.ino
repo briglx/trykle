b31eed922d2f47b5432bc998d081f804c1d9d5bf
Brig Lamoreaux <bklamoreaux@gmail.com>
2021-07-09 16:24:14 -0700

    Add ntp sync

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
+#include <WiFiUdp.h>
+
 #include ".\conf.h"
 #include ".\function.h"
 #include "I2CRTC.h"
@@ -10,10 +12,15 @@ using namespace std;
 
 WiFiServer server(80);
 
-
-#define RTC_SYNC_INTERVAL				3607
+#define NTP_SYNC_INTERVAL 86413L 	// Once a day
+#define RTC_SYNC_INTERVAL	3607 // once an hour
 
 int value = 0;
+
+static const char ntpServerName[] = "us.pool.ntp.org";
+const int timeZone = -7;
+WiFiUDP Udp;
+unsigned int localPort = 8888;  // local port to listen for UDP packets
 
 void setup_wifi() {
 
@@ -65,7 +72,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
       blinkLight(5,10*1000);
   }
   else {
-    Serial.print("Unknown MQTT value");
+    Serial.println("Unknown MQTT value");
   }
 
 }
@@ -94,6 +101,74 @@ void reconnect() {
   }
 }
 
+/*-------- NTP code ----------*/
+
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+
+time_t getNtpTime()
+{
+  IPAddress ntpServerIP; // NTP server's ip address
+
+  while (Udp.parsePacket() > 0) ; // discard any previously received packets
+  // get a random server from the pool
+  WiFi.hostByName(ntpServerName, ntpServerIP);
+  sendNTPpacket(ntpServerIP);
+
+  ulong timeout = millis()+5000;
+  while(millis() < timeout) {
+    if(Udp.parsePacket()) {
+      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      unsigned long secsSince1900;
+      // convert four bytes starting at location 40 to a long integer
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+    }
+  }
+  return 0; // return 0 if unable to get the time
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(IPAddress &address)
+{
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
+
+void performNtpSync(){
+
+  Serial.println("Syncing NTP..");
+  ulong t = getNtpTime();
+  Serial.println(t);
+
+  if (t>0) {
+
+    setTime(t);
+    RTC.set(t);
+
+  }
+
+}
+
 void setup() {
   Serial.begin(115200);
   
@@ -103,6 +178,12 @@ void setup() {
   turnLightOff();
 
   setup_wifi();
+
+  // Start UDP
+  Serial.println("Starting UDP");
+  Udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(Udp.localPort());
   
   // Start the server
   server.begin();
@@ -112,15 +193,10 @@ void setup() {
   client.setServer(mqtt_server, mqttPort);
   client.setCallback(callback);
 
-
   // // Set RTC as the time source
-  // setSyncInterval(RTC_SYNC_INTERVAL);
-  // setSyncProvider(getRtcTime);
+  setSyncInterval(RTC_SYNC_INTERVAL);
+  setSyncProvider(RTC.get);
 
-  Serial.println("Getting Time");
-  time_t cur_time;
-  cur_time = RTC.get();
-  Serial.println(cur_time);
   
 }
 
@@ -129,89 +205,100 @@ void setup() {
 
 void loop() {
 
+  time_t curr_time = now();
+
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
 
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
+
+  if (curr_time % NTP_SYNC_INTERVAL == 0){
+    performNtpSync();
   }
 
-  // Wait until the client sends some data
-  Serial.println("new client");
-  while(!client.available()){
-    delay(1);
-  }
+  Serial.println(now());
+  
+  delay(20000);
 
-  // Read the first line of the request
-  String request = client.readStringUntil('\r');
-  Serial.println(request);
-  client.flush();
+//   // Check if a client has connected
+//   WiFiClient client = server.available();
+//   if (!client) {
+//     return;
+//   }
 
-  // Match the request
+//   // Wait until the client sends some data
+//   Serial.println("new client");
+//   while(!client.available()){
+//     delay(1);
+//   }
+
+//   // Read the first line of the request
+//   String request = client.readStringUntil('\r');
+//   Serial.println(request);
+//   client.flush();
+
+//   // Match the request
  
-  int value = LOW;
-  char* msg;
-  if (request.indexOf("/LED=ON") != -1)  {
-    // digitalWrite(LED_BUILTIN, LOW);
-    // digitalWrite(LED_PIN, HIGH);
-    turnLightOn();
-    value = HIGH;
-    msg = "High";
-  }
-  else if (request.indexOf("/LED=OFF") != -1)  {
-    // digitalWrite(LED_BUILTIN, HIGH);
-    // digitalWrite(LED_PIN, LOW);
-    turnLightOff();
-    value = LOW;
-    msg = "Low";
-  }
-  else if (request.indexOf("/BLINK=10") != -1)  {
-    blinkLight(5,10*1000);
-    value = LOW;
-    msg = "Low";
-  }
-  else if (request.indexOf("/BLINK=5") != -1)  {
-    blinkLight(5,5*1000);
-    value = LOW;
-    msg = "Low";
-  }
-  else if (request.indexOf("/BLINK=1") != -1)  {
-    blinkLight(5,1*1000);
-    value = LOW;
-    msg = "Low";
-  }
-  else {
-    msg = "Unknown";
-  }
+//   int value = LOW;
+//   char* msg;
+//   if (request.indexOf("/LED=ON") != -1)  {
+//     // digitalWrite(LED_BUILTIN, LOW);
+//     // digitalWrite(LED_PIN, HIGH);
+//     turnLightOn();
+//     value = HIGH;
+//     msg = "High";
+//   }
+//   else if (request.indexOf("/LED=OFF") != -1)  {
+//     // digitalWrite(LED_BUILTIN, HIGH);
+//     // digitalWrite(LED_PIN, LOW);
+//     turnLightOff();
+//     value = LOW;
+//     msg = "Low";
+//   }
+//   else if (request.indexOf("/BLINK=10") != -1)  {
+//     blinkLight(5,10*1000);
+//     value = LOW;
+//     msg = "Low";
+//   }
+//   else if (request.indexOf("/BLINK=5") != -1)  {
+//     blinkLight(5,5*1000);
+//     value = LOW;
+//     msg = "Low";
+//   }
+//   else if (request.indexOf("/BLINK=1") != -1)  {
+//     blinkLight(5,1*1000);
+//     value = LOW;
+//     msg = "Low";
+//   }
+//   else {
+//     msg = "Unknown";
+//   }
 
-  // Return the response
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println(""); //  do not forget this one
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>");
+//   // Return the response
+//   client.println("HTTP/1.1 200 OK");
+//   client.println("Content-Type: text/html");
+//   client.println(""); //  do not forget this one
+//   client.println("<!DOCTYPE HTML>");
+//   client.println("<html>");
  
-  client.print("Led pin is now: ");
-  client.print(msg);
-//  if(value == HIGH) {
-//    client.print("On");
-//  } else {
-//    client.print("Off");
-////  }
-  client.println("<br><br>");
-  client.println("<a href=\"/LED=ON\"\"><button>Turn On</button></a>");
-  client.println("<a href=\"/LED=OFF\"\"><button>Turn Off</button></a><br />");
-  client.println("<a href=\"/BLINK=1\"\"><button>Blink 1 Sec</button></a><br />"); 
-  client.println("<a href=\"/BLINK=5\"\"><button>Blink 5 Sec</button></a><br />"); 
-  client.println("<a href=\"/BLINK=10\"\"><button>Blink 10 Sec</button></a><br />");  
-  client.println("</html>");
+//   client.print("Led pin is now: ");
+//   client.print(msg);
+// //  if(value == HIGH) {
+// //    client.print("On");
+// //  } else {
+// //    client.print("Off");
+// ////  }
+//   client.println("<br><br>");
+//   client.println("<a href=\"/LED=ON\"\"><button>Turn On</button></a>");
+//   client.println("<a href=\"/LED=OFF\"\"><button>Turn Off</button></a><br />");
+//   client.println("<a href=\"/BLINK=1\"\"><button>Blink 1 Sec</button></a><br />"); 
+//   client.println("<a href=\"/BLINK=5\"\"><button>Blink 5 Sec</button></a><br />"); 
+//   client.println("<a href=\"/BLINK=10\"\"><button>Blink 10 Sec</button></a><br />");  
+//   client.println("</html>");
 
-  delay(1);
-  Serial.println("Client disonnected");
-  Serial.println("");
+//   delay(1);
+//   Serial.println("Client disonnected");
+//   Serial.println("");
  
 }
